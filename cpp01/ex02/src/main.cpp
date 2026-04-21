1c84e1163d2554e788b7f302e90098d91420c8f5
rafa <rafa@pop-os.home>
2026-04-21 12:29:22 +0200

    finished cpp01 ex02

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -9,27 +9,36 @@ int	main(void)
 	std::string&	stringREF = Brain;
 
 	//Address of the string variable
+	std::cout << "Address of the string variable" << std::endl;
 	std::cout << &Brain << std::endl;
 	// Address held by stringPTR
+	std::cout << "Address held by stringPTR" << std::endl;
 	std::cout << stringPTR << std::endl;
 	// Address held by stringREF
+	std::cout << "Address held by stringREF" << std::endl;
 	std::cout << &stringREF << std::endl;
 
 	// Value of the string variable
+	std::cout << "Value of the string variable" << std::endl;
 	std::cout << Brain << std::endl;
 	// Value pointed to by stringPTR
+	std::cout << "Value pointed to by stringPTR" << std::endl;
 	std::cout << *stringPTR << std::endl;
 	// Value pointed to by stringREF
+	std::cout << "Value pointed to by stringREF" << std::endl;
 	std::cout << stringREF << std::endl;
 
     //Change to reference
     *stringPTR = "PATATA";
 
     // Value of the string variable
+	std::cout << "Value of the string variable" << std::endl;
 	std::cout << Brain << std::endl;
 	// Value pointed to by stringPTR
+	std::cout << "Value pointed to by stringPTR" << std::endl;
 	std::cout << *stringPTR << std::endl;
 	// Value pointed to by stringREF
+	std::cout << "Value pointed to by stringREF" << std::endl;
 	std::cout << stringREF << std::endl;
 	return (0);
 }
